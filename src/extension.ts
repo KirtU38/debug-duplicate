2d91b5d72fe0c80b0503afb64012db28c7f2989a
KirtU38 <belocegor@gmail.com>
2023-06-19 12:51:33 +0200

    v4 release

--- src/extension.ts
+++ src/extension.ts
@@ -15,15 +15,30 @@ export function activate(context: vscode.ExtensionContext) {
     let spacesToInsertBeforeTheDuplicatedLine = selectedLineRawSplit[0].match(/\s+/) ? selectedLineRawSplit[0] : "";
     let selectedLine = editor.document.lineAt(selection.start.line).text.trim();
     let debugWithVatiableInside = /^System\.debug\(\w.*\).*/gi;
-    let variableDeclaration = /.+=.+;.*/g;
+    let variableDeclaration = /.+=.+(;|\)).*/g;
     let methodDeclaration = /(public|private|protected).+\(.*/gi;
-    let ifStatement = /\s*if\s*\(..*/gi;
-    let blankLine = /\s*/gi;
+    let ifStatement = /\s*if\s*\(.*/gi;
+    let blankLine = /^\s*$/gi;
 
     let duplicatedLine = "";
     if (selectedLine.match(debugWithVatiableInside)) {
+      console.log('selectedLine.match(debugWithVatiableInside)');
       duplicatedLine = selectedLine.replace(/^System\.debug\((.+)\).*/gi, `System.debug('$1')`);
-    } else if (selectedLine.match(variableDeclaration)) {
+    } else if (selectedLine.match(ifStatement)) {
+      console.log('selectedLine.match(ifStatement)');
+      let ifStatementWithoutCurlyBrace = selectedLine.replace('{', '').trim();
+      spacesToInsertBeforeTheDuplicatedLine += ' '.repeat(4);
+      duplicatedLine = `System.debug('--> ${ifStatementWithoutCurlyBrace}');`;
+    } else if (selectedLine.match(methodDeclaration)) {
+      console.log('selectedLine.match(methodDeclaration)');
+      let methodDeclarationSplit = selectedLine.split('(');
+      let methodSignature = methodDeclarationSplit[0].trim();
+      let methodSignatureSplit = methodSignature.split(/\s+/);
+      let methodName = methodSignatureSplit[methodSignatureSplit.length - 1];
+      spacesToInsertBeforeTheDuplicatedLine += ' '.repeat(4);
+      duplicatedLine = `System.debug('${methodName}()');`;
+    } else if(selectedLine.match(variableDeclaration)) {
+      console.log('selectedLine.match(variableDeclaration)');
       let selectedLineSplit = selectedLine.split('=');
       let declarationPart = selectedLineSplit[0].trim();
       let declarationPartSplit = declarationPart.split(/\s+/);
@@ -34,20 +49,11 @@ export function activate(context: vscode.ExtensionContext) {
         variableName = declarationPartSplit[0].trim();
       }
       duplicatedLine = `System.debug(${variableName});`;
-    } else if (selectedLine.match(methodDeclaration)) {
-      let methodDeclarationSplit = selectedLine.split('(');
-      let methodSignature = methodDeclarationSplit[0].trim();
-      let methodSignatureSplit = methodSignature.split(/\s+/);
-      let methodName = methodSignatureSplit[methodSignatureSplit.length - 1];
-      spacesToInsertBeforeTheDuplicatedLine += ' '.repeat(4);
-      duplicatedLine = `System.debug('${methodName}()');`;
-    } else if(selectedLine.match(ifStatement)) {
-      let ifStatementWithoutCurlyBrace = selectedLine.replace('{', '').trim();
-      spacesToInsertBeforeTheDuplicatedLine += ' '.repeat(4);
-      duplicatedLine = `System.debug('--> ${ifStatementWithoutCurlyBrace}');`;
     } else if(selectedLine.match(blankLine)) {
+      console.log('selectedLine.match(blankLine)');
       duplicatedLine = `System.debug('Pointer: ${counter++}');`;
     } else {
+      console.log('STANDARD');
       duplicatedLine = `System.debug('${selectedLine}');`;
     }
 
